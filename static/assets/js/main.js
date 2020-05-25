8ea75d4b40fb9a10e837dc263746a5b8e71dc17d
Aigars Reiters <straygar@gmail.com>
2020-05-25 19:20:32 +0100

    Add blog link

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 	
-	$('ul.tabs li').click(function(){
-		var tab_id = $(this).attr('data-tab');
+	// $('ul.tabs li').click(function(){
+	// 	var tab_id = $(this).attr('data-tab');
 		
-		$('ul.tabs li').removeClass('selected');
-		$('.content').removeClass('selected');
+	// 	$('ul.tabs li').removeClass('selected');
+	// 	$('.content').removeClass('selected');
 
-		$(this).addClass('selected');
-		$("#"+tab_id).addClass('selected');
-	})
+	// 	$(this).addClass('selected');
+	// 	$("#"+tab_id).addClass('selected');
+	// })
 
 })
